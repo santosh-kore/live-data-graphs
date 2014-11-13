0f8f336c58b5bb5e42a4762382a1687952cbeb88
santosh-kore <sbkore_andh@yahoo.com>
2014-11-13 11:24:56 +0530

    Removing files

--- watcher.js
+++
@@ -1,9 +0,0 @@
-var Watcher = require('file-watch');
-
-var watcher = new Watcher();
-
-watcher.watch('custom event', [__dirname + "data/graph-data.txt"]);
-
-watcher.on('custom event', function () {
-  console.log('custom event!')
-});
